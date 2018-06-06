b7df1effbfe217c5fcc2d7548cf030af9eeb4015
bomi <a51022866@gmail.com>
2018-06-06 21:30:53 +0900

    example_SELECT_2

--- JDBC/jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
+++ JDBC/jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import kr.or.connect.jdbcexam.dto.Role;
 
 public class RoleDao {
@@ -50,7 +52,7 @@ public class RoleDao {
           e.printStackTrace();
         }
       }
-      
+
       if(conn != null) {
         try {
           conn.close();
@@ -58,34 +60,66 @@ public class RoleDao {
           e.printStackTrace();
         }
       }
-      
+
     }
     return role;
   }
-  
+
   // INSERT - 하나의 데이터
   public int addRole(Role role) {
     int insertCount = 0;
-    
+
     try {
       Class.forName("com.mysql.jdbc.Driver");
     } catch(ClassNotFoundException e) {
       e.printStackTrace();
     }
-    
+
     String sql = "INSERT INTO role(role_id, description) VALUES (?, ?)";
-    
+
     try(Connection conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
         PreparedStatement ps = conn.prepareStatement(sql)) {
-      
+
       ps.setInt(1, role.getRoleId());
       ps.setString(2, role.getDescription());
-      
-      insertCount = ps.executeUpdate();
-      
+
+      insertCount = ps.executeUpdate(); // INSERT, DELETE, UPDATE문을 실행할때 사용
+
     } catch(Exception ex) {
-       ex.printStackTrace();
+      ex.printStackTrace();
     }
     return insertCount;
+  }
+
+  // SELECT - 여러개의 데이터
+  public List<Role> getRoles() {
+    List<Role> list = new ArrayList<>();
+
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+    } catch(ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    String sql = "SELECT * FROM role ORDER BY role_id DESC";
+
+    try(Connection conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+
+      try(ResultSet rs = ps.executeQuery()) {
+
+        while(rs.next()) {
+          int id = rs.getInt(1);
+          String description = rs.getString("description");
+          Role role = new Role(id, description);
+          list.add(role);
+        }
+      } catch(Exception e) {
+        e.printStackTrace();
+      }
+    } catch(SQLException ex) {
+      ex.printStackTrace();
+    }
+    return list;
   }
 }
