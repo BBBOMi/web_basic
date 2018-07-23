ca6a9b4eb1a1b0c2821ec4c96efba4d837da72ec
bomi <a51022866@gmail.com>
2018-07-23 21:26:45 +0900

    query_insert

--- Spring/JDBC/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDao.java
+++ Spring/JDBC/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDao.java
@@ -6,7 +6,10 @@ import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import kr.or.connect.daoexam.dto.Role;
@@ -15,10 +18,12 @@ import static kr.or.connect.daoexam.dao.RoleDaoSqls.*;
 @Repository
 public class RoleDao {
   private NamedParameterJdbcTemplate jdbc; 
+  private SimpleJdbcInsert insertAction;
   private RowMapper<Role> rowMapper = BeanPropertyRowMapper.newInstance(Role.class);
   
   public RoleDao(DataSource dataSource) {
     this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+    this.insertAction = new SimpleJdbcInsert(dataSource).withTableName("role");
   }
   
   public List<Role> selectAll() {
@@ -26,5 +31,10 @@ public class RoleDao {
     // Collections.emptyMap() : sql문에 바인딩할 값이 있을 경우에, 이 바인딩할 값을 전달할 목적으로 사용
     // rowMapper : select 한 건 한 건을 dto에 저장할 목적으로 사용. BeanPropertyRowMapper 객체를 이용해서 컬럼의 값을 자동으로 dto에 담아준다.
     // BeanPropertyRowMapper는 결과가 여러 건이었을 때, 내부적으로 반복해서 dto를 생성. 생성한 dto를 list에 담아주고 해당 list를 반환
+  }
+  
+  public int insert(Role role) {
+    SqlParameterSource params = new BeanPropertySqlParameterSource(role);
+    return insertAction.execute(params);
   }
 }
