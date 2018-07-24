bc1f29eeecbea6067e2c9d81ebec9756bdaa838b
bomi <a51022866@gmail.com>
2018-07-24 21:11:42 +0900

    query_delete_selectById

--- Spring/JDBC/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDao.java
+++ Spring/JDBC/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDao.java
@@ -2,8 +2,10 @@ package kr.or.connect.daoexam.dao;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import javax.sql.DataSource;
-
+import javax.swing.plaf.BorderUIResource.EmptyBorderUIResource;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -41,5 +43,20 @@ public class RoleDao {
   public int update(Role role) {
     SqlParameterSource params = new BeanPropertySqlParameterSource(role);
     return jdbc.update(UPDATE, params);
+  }
+  
+  public int deleteById(Integer id) {
+    Map<String, ?> params = Collections.singletonMap("roleId", id);
+    return jdbc.update(DELETE_BY_ROLE_ID, params);
+  }
+  
+  public Role selectById(Integer id) {
+    try {
+      Map<String, ?> params = Collections.singletonMap("roleId", id);
+      return jdbc.queryForObject(SELECT_BY_ROLE_ID, params, rowMapper);
+    } catch(EmptyResultDataAccessException e) {
+      // 찾는 id값이 없을 경우
+      return null;
+    }
   }
 }
--- Spring/JDBC/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDaoSqls.java
+++ Spring/JDBC/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDaoSqls.java
@@ -3,4 +3,7 @@ package kr.or.connect.daoexam.dao;
 public class RoleDaoSqls {
   public static final String SELECT_ALL = "SELECT role_id, description FROM role ORDER BY role_id";
   public static final String UPDATE = "UPDATE role SET description = :description WHERE role_id = :roleId";
+  public static final String SELECT_BY_ROLE_ID = "SELECT role_id, description FROM role WHERE role_id = :roleId";
+  // 전체 칼럼 내용을 출력하더라도 칼럼명을 명시해주는 것이 좋음
+  public static final String DELETE_BY_ROLE_ID = "DELETE FROM role WHERE role_id = :roleId";
 }
